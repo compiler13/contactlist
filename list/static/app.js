df5f0b1aa206439542d0f7ad01efe3c5c230ce61
Enamul Haque <shawoncse12@gmail.com>
2018-04-04 19:26:51 +0600

    Update app.js

--- list/static/app.js
+++ list/static/app.js
@@ -1,26 +1,72 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope, $http) {
+    $scope.form = {};
 
 
     // $http.get("http://127.0.0.1:8000/contact-list/")
     // .then(function(response) {
     //     $scope.data = response.data; 
     // });
- $http({
+ 
+$scope.postContact = function () {
+    console.log("hello form post");
+    console.log($scope.form);
+    $http({
 
-method : "POST",
-url : "",
+        method : "POST",
+        
+        url : "http://127.0.0.1:8000/contact-list/",
+        
+        data: $scope.form
+        
+        
+         }).then(function(response) {
+            $scope.data = response.data;
+        }).catch(function(error) {
+            console.log("Error", error);
+        })
+} 
+$scope.getContactList = function () {
+    $http({
 
-data : {name: 'shawon' }
-
-
- }).then(function(response)
-
-{
-    $scope.name = response.data;
+        method : "GET",
+        
+        url : "http://127.0.0.1:8000/contact-list/",
+        
+        // data : {name: 'shawon' }
+        
+        
+         }).then(function(response) {
+             console.log("helllo");
+            $scope.data = response.data;
+        }).catch(function(error) {
+            console.log("Error", error);
+        }) 
 }
+$scope.getContactList();
+    
+});
 
-)
+app.controller('postCtrl', function($scope, $http) {
+    $scope.form = {};
+
+$scope.postContact = function () {
+    console.log("hello form post");
+    console.log($scope.form);
+    $http({
+    method : "POST",
+    
+    url : "http://127.0.0.1:8000/contact-list/",
+    
+    data: $scope.form
+    
+    
+        }).then(function(response) {
+        $scope.data = response.data;
+    }).catch(function(error) {
+        console.log("Error", error);
+    })
+} 
     
 });
